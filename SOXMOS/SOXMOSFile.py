7b3d88efc10021dd9abf840d953401370bac8be3
Dominik Stańczak <stanczakdominik@gmail.com>
2022-04-08 13:32:24 +0200

    Pass savgol as dict

--- SOXMOS/SOXMOSFile.py
+++ SOXMOS/SOXMOSFile.py
@@ -16,6 +16,14 @@ memflag = os.environ.get("TEST_SOXMOS", False)
 location = None if memflag else "./cachedir"
 memory = Memory(location, verbose=0)
 
+savgol_settings = {
+    (19.5177, 34.3053): {"savgol_window_length": 13, "savgol_polyorder": 4},
+    (75.7912, 101.6328): {"savgol_window_length": 14, "savgol_polyorder": 4},
+    (1.3272, 3.5807): {"savgol_window_length": 13, "savgol_polyorder": 4},
+    (2.6977, 5.4807): {"savgol_window_length": 13, "savgol_polyorder": 4},
+    (25.1769, 32.0582): {"savgol_window_length": 13, "savgol_polyorder": 4},
+}
+
 
 class SOXMOSFile:
     def __init__(
@@ -26,7 +34,6 @@ class SOXMOSFile:
         self.savgol_settings = dict(
             window_length=savgol_window_length, polyorder=savgol_polyorder
         )
-        print(f"Savgol settings for {self.shotid}: {self.savgol_settings}")
 
     @property
     def description(self):
@@ -42,6 +49,13 @@ class SOXMOSFile:
 
     def __repr__(self, *args, **kwargs):
         return f"{self.__class__.__name__}({self.description})"
+
+    @cached_property
+    def Lambda_interval(self):
+        Lambda = self.dataset.Rough_wavelength
+        min1, min2 = Lambda.min(dim="pixel").data
+        max1, max2 = Lambda.max(dim="pixel").data
+        return {1: (min1, max1), 2: (min2, max2)}
 
     def plot_spectrogram(self, *, vmax=None):
         fig = self.dataset.FilteredCount.plot(
@@ -138,13 +152,21 @@ def parse_dataset(dataframe, config, savgol_settings):
     ds = ds.set_coords("Rough_wavelength")
 
     ds["Count"] = ds.Count.where(ds.Count.median("Time") != 0, np.nan)
-    ds["FilteredCount"] = xarray.apply_ufunc(
-        signal.savgol_filter,
-        ds.Count,
-        kwargs=savgol_settings,
-        input_core_dims=[["pixel"]],
-        output_core_dims=[["pixel"]],
-    )
+
+    def filter_helper(group):
+        Lambda = group.Rough_wavelength
+        interval = (Lambda.min().item(), Lambda.max().item())
+        local_savgol_settings = savgol_settings[interval]
+
+        return xarray.apply_ufunc(
+            signal.savgol_filter,
+            group.Count,
+            kwargs=local_savgol_settings,
+            input_core_dims=[["pixel"]],
+            output_core_dims=[["pixel"]],
+        )
+
+    ds["FilteredCount"] = ds.groupby("ch").apply(filter_helper)
     ds["FilteredCount"].attrs.update(savgol_settings)
     for key in ["Count", "FilteredCount"]:
         ds[key].attrs["units"] = "counts"
